53591c0bdaf20ec79b114446936949b23aa74613
sashi0034 <satoshi3104.nishi@gmail.com>
2023-09-16 08:52:14 +0900

    コマンドライン読み取りを実装

---
+++ NkDl/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿namespace NkDl;
+
+static class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        ProgramArgs programArgs;
+        try
+        {
+            programArgs = ProgramArgs.Parse(args);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            Console.WriteLine(ProgramArgs.UsageDescription);
+            return;
+        }
+
+        Console.WriteLine(programArgs.Url);
+    }
+}
---
+++ NkDl/ProgramArgs.cs
@@ -0,0 +1,35 @@
+﻿namespace NkDl;
+
+public record ProgramArgs(
+    string Url = "")
+{
+    public const string UsageDescription = @"
+Usage: nkdl <url>
+";
+
+    public static ProgramArgs Parse(string[] arg)
+    {
+        var result = new ProgramArgs();
+
+        for (int i = 0; i < arg.Length; ++i)
+        {
+            result = processNext(arg, result, i);
+        }
+
+        if (string.IsNullOrWhiteSpace(result.Url)) throw new ArgumentException("URL not specified");
+
+        return result;
+    }
+
+    private static ProgramArgs processNext(string[] arg, ProgramArgs result, int nextIndex)
+    {
+        string next = arg[nextIndex];
+
+        if (nextIndex == arg.Length - 1 && next.StartsWith("-") == false)
+        {
+            result = result with { Url = arg[0] };
+        }
+
+        return result;
+    }
+}
